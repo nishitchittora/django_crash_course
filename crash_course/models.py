0fa9ffc9b81a274d5c50ceca73b2d7935be1eda6
nishitchittora <nishitchittora2015@gmail.com>
2020-12-20 13:58:15 +0530

    SectionContent model deleted

--- crash_course/models.py
+++ crash_course/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from .mixins import StatusMixin, TimeStampedModel, TitleSlugMixin
-from django.template.defaultfilters import slugify
 
 # Create your models here.
 
@@ -23,13 +22,6 @@ class CourseChapter(TimeStampedModel, StatusMixin, TitleSlugMixin):
 class ChapterSection(TimeStampedModel, StatusMixin, TitleSlugMixin):
     description =  models.TextField(blank=True, null=True)
     chapter = models.ForeignKey(CourseChapter, null=True, blank=True, on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return self.title
-
-
-class SectionContent(TimeStampedModel, StatusMixin, TitleSlugMixin):
-    section = models.ForeignKey(ChapterSection, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
