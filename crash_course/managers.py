5db541a631984a51206f99b8bb97e8cd91a079b9
nishitchittora <nishitchittora2015@gmail.com>
2020-12-20 13:58:47 +0530

    managers updated

--- crash_course/managers.py
+++ crash_course/managers.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class StatusMixinManager(models.Manager):
     def all(self, *args, **kwargs):
-        return super(StatusMixinManager, self).filter(is_deleted=False)
+        return super(StatusMixinManager, self).filter(is_active=True, is_deleted=False)
 
     def filter(self, *args, **kwargs):
         return super(StatusMixinManager, self).filter(is_active=True, is_deleted=False).filter(*args, **kwargs)
